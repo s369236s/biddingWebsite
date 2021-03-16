e0b843234a4a9eba7569acfd6c833ab7ac34a1db
s369236s <s369236s@gmail.com>
2021-03-16 20:55:13 +0800

    0

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const io = require("socket.io")(server, {
     origin: "http://localhost:3000",
   },
 });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const url = "mongodb://127.0.0.1:27017";
 
